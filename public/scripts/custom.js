a914168c39447c8bf95e506a03fd00ea198aba25
Ben Greenier <ben@bengreenier.com>
2016-05-03 00:03:25 -0400

    ui logic

---
+++ public/scripts/custom.js
@@ -0,0 +1,24 @@
+var username;
+
+$(document).ready(function () {
+	$('.available').hide();
+	$('.unavailable').hide();
+
+	username = $('#username').text();
+	console.log(username);
+
+	var servicesList = $('.loading');
+	$.each(servicesList, function(service) {
+		var $this = $(this);
+		var svc = $this.data("svc");
+		$.get( "/availability/" + svc + "?username=" + username, function (res) {
+			$this.find(".loading-spinner").fadeOut(function () {
+				if (res[svc]) {
+					$this.find(".available").fadeIn();
+				} else {
+					$this.find(".unavailable").fadeIn();
+				}
+			});
+		});
+	});
+});
